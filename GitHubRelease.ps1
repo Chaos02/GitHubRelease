9f1f315762c712cd610bf93eb666e0fc000f200c
Chaos02 <mknoack@gmx.de>
2022-02-17 19:25:59 +0100

    Initial Script, untested

--- GitHubRelease.ps1
+++ GitHubRelease.ps1
@@ -5,14 +5,14 @@ Create GitHub releases through a script.
 Uses the GitHub API to create releases, complete with changelog etc.
 .PARAMETER debug
 Turns on PowerShell debugging mode.
-.PARAMETER APIKey
-Mandatory. Generate on https://authors.curseforge.com/account/api-tokens.
+.PARAMETER Token
+Mandatory. Generate on https://github.com/settings/tokens with full Repo access.
 .PARAMETER ProjectID
 Mandatory. Provide the ProjectID of your project on CurseForge as a string.
 .PARAMETER File
 Mandatory. Provide the path to the file to be uploaded to the project.
 .PARAMETER GameVersions
-Mandatory. Provide a string array of supported game Versions. See https://support.curseforge.com/en/support/solutions/articles/9000197321-curseforge-upload-api#:~:text=JavaScript-,Game%20Versions%20API,-To%20retrieve%20a
+Mandatory. Provide a string array of supported game Versions. See 
 .PARAMETER ChangeLogType
 Provide the type of changelog: One of "text", "html", "markdown".
 Default: 'text'
@@ -24,16 +24,32 @@ Provide a String of change notes.
 #>
 
 param(
-	[Parameter(Mandatory)][string]$APIKey,
-	[Parameter(Mandatory)][string]$ProjectID,
+	[Parameter(Mandatory)][string]$Token=(Get-Content -Raw -Path ((Split-Path -Parent $PSScriptRoot) + '/GitHub_token.key')),
+	[Parameter(Mandatory)][string]$Owner,
+	[Parameter(Mandatory)][string]$Repo,
 	[Parameter(Mandatory)][string]$File,
 	[Parameter(Mandatory)][string[]]$GameVersions,
-	[string]$ChangeLogType='text',
-	[string]$ReleaseType='release',
-	[string]$ChangeLog=''
+	[switch]$PreRelease=$false,
+	[switch]$Draft=$false,
+	[string]$ChangeLog='',
+	[switch]$GenReleaseNotes=$true
 )
 
 if ($DebugPreference) {
 	Set-PSDebug -Trace 2
 }
+
+$Body = @{
+	'accept' 		= 'application/vnd.github.v3+json'
+	'owner' 			= "$Owner"
+	'repo' 			= "$Repo"
+	'tag_name' 		= "$TagName"
+	'name' 			= "$File"
+	'body' 			= "$ChangeLog"
+	'draft' 			= $Draft
+	'prerelease' 	= $PreRelease
+	'genereate_release_notes' = $GenReleaseNotes;
+}
+
+curl -u $Owner:$Token -X POST -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/$Owner/$Repo/releases -d "{$Body}"
 

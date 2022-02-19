2e4da871fe833460e756a96b1274385551915651
Chaos02 <mknoack@gmx.de>
2022-02-19 02:49:31 +0100

    added proper help page

--- GitHubRelease.ps1
+++ GitHubRelease.ps1
@@ -5,30 +5,41 @@ Create GitHub releases through a script.
 Uses the GitHub API to create releases, complete with changelog etc.
 .PARAMETER debug
 Turns on PowerShell debugging mode.
+.PARAMETER Owner
+(Mandatory) Owner name of the repository. Can also be supplied through -OwnRep.
+.PARAMETER Repo
+(Mandatory) Repository name. Can also be supplied through -OwnRep.
+.PARAMETER OwnRep
+(Mandatory) Meant to be supplied by GitHub Workflow Actions in format.
 .PARAMETER Token
 Mandatory. Generate on https://github.com/settings/tokens with full Repo access.
-.PARAMETER ProjectID
-Mandatory. Provide the ProjectID of your project on CurseForge as a string.
+Either String or path to a file containing the token.
 .PARAMETER File
 Mandatory. Provide the path to the file to be uploaded to the project.
-.PARAMETER GameVersions
-Mandatory. Provide a string array of supported game Versions. See 
-.PARAMETER ChangeLogType
-Provide the type of changelog: One of "text", "html", "markdown".
-Default: 'text'
-.PARAMETER ReleaseType
-Provide the type of release: One of "alpha", "beta", "release".
-Default: 'release'
+.PARAMETER TagName
+Mandatory. Provide GitHub release tag.
+.PARAMETER PreRelease
+Create as a prerelease instead of regular release.
+Default: 'false'
+.PARAMETER Draft
+Wether or not to make the release a draft rather than a regular release.
+Default: 'false'
 .PARAMETER ChangeLog
-Provide a String of change notes.
+Provide a String of change notes that is prefixed to GitHub's automatic ones. See -GenReleaseNotes.
+.PARAMETER GenReleaseNotes
+Wether or not to auto generate release notes from commit messages.
+Default: 'true'
 #>
 
 param(
-	[Parameter(Mandatory)][string]$Token=(Get-Content -Raw -Path ((Split-Path -Parent $PSScriptRoot) + '/GitHub_token.key')),
-	[Parameter(Mandatory)][string]$Owner,
-	[Parameter(Mandatory)][string]$Repo,
+	[CmdletBinding(DefaultParameterSetName = 'Manual')]
+
+	[Parameter(Mandatory, ParameterSetName = 'Manual')][string]$Owner,
+	[Parameter(Mandatory, ParameterSetName = 'Manual')][string]$Repo,
+	[Parameter(Mandatory, ParameterSetName = 'Auto')][string]$OwnRep,
+	[Parameter(Mandatory)][string]$Token,
 	[Parameter(Mandatory)][string]$File,
-	[Parameter(Mandatory)][string[]]$GameVersions,
+	[Parameter(Mandatory)][string]$TagName,
 	[switch]$PreRelease=$false,
 	[switch]$Draft=$false,
 	[string]$ChangeLog='',
@@ -37,6 +48,22 @@ param(
 
 if ($DebugPreference) {
 	Set-PSDebug -Trace 2
+}
+
+if ( $OwnRep ) {
+	$OwnRep -match '.*(?=\/[^\/]*)' *>$Null
+	$Owner = $Matches[0]
+	$Matches.Clear()
+	$OwnRep -match '(?<=[^\/]*\/).*' *>$Null
+	$Repo = $Matches[0]
+	$Matches.Clear()
+} else {
+	Write-Host 'Using Manual Repo information, pipe github.repository into $OwnRep for automatic.'
+}
+
+if ( Test-Path $Token -PathType Leaf ) {
+	Write-Host 'Reading token from file: $Token'
+	$Token = (Get-Content -Raw -Path ((Split-Path -Parent $PSScriptRoot) + '/' + $Token))
 }
 
 $Body = @{
